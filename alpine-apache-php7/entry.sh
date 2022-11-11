1460128b5f464a7b3221153f5400d864fd073822
Oliver Guo <oliguo@users.noreply.github.com>
2022-11-11 16:56:24 +0800

    Update entry.sh
    add mssql support

--- alpine-apache-php7/entry.sh
+++ alpine-apache-php7/entry.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+echo extension=pdo_sqlsrv.so >> `php --ini | grep "Scan for additional .ini files" | sed -e "s|.*:\s*||"`/10_pdo_sqlsrv.ini
+echo extension=sqlsrv.so >> `php --ini | grep "Scan for additional .ini files" | sed -e "s|.*:\s*||"`/20_sqlsrv.ini
+
 umask 0000
 ##Start crond
 /usr/sbin/crond start
-/usr/bin/crontab -u apache /opt/crontab.conf
+/usr/bin/crontab /opt/crontab.conf
 
 ##Start httpd
 /usr/sbin/httpd -D FOREGROUND
