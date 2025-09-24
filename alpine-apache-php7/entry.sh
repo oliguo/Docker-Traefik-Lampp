6fec823f73094c8050d2b1700e94c65567024766
Oliver Guo <oliguo@users.noreply.github.com>
2025-09-24 13:45:32 +0800

    Update entry.sh

--- alpine-apache-php7/entry.sh
+++ alpine-apache-php7/entry.sh
@@ -6,6 +6,6 @@ umask 0022
 ##Start crond
 /usr/sbin/crond start
 /usr/bin/crontab -u apache /opt/crontab.conf
-chown -R 100:101 /var/www/localhost/htdocs/
+chown -R apache:apache /var/www/localhost/htdocs/
 ##Start httpd
 /usr/sbin/httpd -D FOREGROUND
