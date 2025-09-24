c1b059925eb68426789a1b14233ad47ea0d0460c
Oliver Guo <oliguo@users.noreply.github.com>
2025-09-24 14:04:05 +0800

    Update ownership and permissions in entry.sh
    Change ownership and permissions for htdocs directory

--- alpine-apache-php7/entry.sh
+++ alpine-apache-php7/entry.sh
@@ -6,6 +6,7 @@ umask 0022
 ##Start crond
 /usr/sbin/crond start
 /usr/bin/crontab -u apache /opt/crontab.conf
-chown -R apache:apache /var/www/localhost/htdocs/
+chown -R :apache /var/www/localhost/htdocs/
+chmod -R 770 /var/www/localhost/htdocs/
 ##Start httpd
 /usr/sbin/httpd -D FOREGROUND
