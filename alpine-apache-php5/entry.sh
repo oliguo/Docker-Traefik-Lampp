aa1772ea5d425e1e42e69dbca59d9d64d896c947
Oliver Guo <oliguo@users.noreply.github.com>
2025-09-24 13:41:09 +0800

    Update entry.sh

--- alpine-apache-php5/entry.sh
+++ alpine-apache-php5/entry.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-umask 0000
+umask 0022
 ##Start crond
 /usr/sbin/crond start
 /usr/bin/crontab -u apache /opt/crontab.conf
-
+chown -R 100:101 /var/www/localhost/htdocs/
 ##Start httpd
 /usr/sbin/httpd -D FOREGROUND
